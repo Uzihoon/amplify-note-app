ed68c5ef184a6744c81be370d92d697cea644752
uzihoon <uzihoon.dev@gmail.com>
2021-07-18 22:32:52 +0900

    Update note view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import API from '@aws-amplify/api';
-import React, { useReducer } from 'react';
+import { List } from 'antd';
+import React, { useEffect, useReducer } from 'react';
 import { listNotes } from './graphql/queries';
+import 'antd/dist/antd.css';
 
 const initialState = {
   notes: [],
@@ -27,9 +29,39 @@ function App() {
       const notesData = await API.graphql({
         query: listNotes
       });
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
     } catch (err) {
       console.log('error: ', err);
       dispatch({ type: 'ERROR' });
     }
   };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
+    </div>
+  );
 }
+
+function renderItem(item) {
+  return (
+    <List.Item style={styles.item}>
+      <List.Item.Meta title={item.name} description={item.description} />
+    </List.Item>
+  );
+}
+
+const styles = {
+  container: { padding: 20 },
+  input: { marginBottom: 10 },
+  item: { textAlign: 'left' },
+  p: { color: '#1809ff' }
+};
