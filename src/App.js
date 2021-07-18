c81cca3ad9158f1f4c9b57ad9115dd5e70bbbc8a
uzihoon <uzihoon.dev@gmail.com>
2021-07-18 22:41:49 +0900

    Update add function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import API from '@aws-amplify/api';
-import { List } from 'antd';
+import { Button, Input, List } from 'antd';
 import React, { useEffect, useReducer } from 'react';
 import { listNotes } from './graphql/queries';
 import 'antd/dist/antd.css';
+import { v4 as uuid } from 'uuid';
+import { createNote as CreateNote } from './graphql/mutations';
 
 const initialState = {
   notes: [],
   loading: true,
   error: false,
-  from: { name: '', description: '' }
+  form: { name: '', description: '' }
 };
+
+const CLIENT_ID = uuid();
 
 function reducer(state, action) {
   switch (action.type) {
@@ -17,6 +21,12 @@ function reducer(state, action) {
       return { ...state, notes: action.notes, loading: false };
     case 'ERROR':
       return { ...state, loading: false, error: true };
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes] };
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form };
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
     default:
       return state;
   }
@@ -24,6 +34,28 @@ function reducer(state, action) {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const createNote = async () => {
+    const { form } = state;
+    if (!form.name || !form.description) {
+      return alert('Please enter a name and description');
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: 'ADD_NOTE', note });
+    dispatch({ type: 'RESET_FORM' });
+
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      });
+      alert('successfully created note!');
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  };
+
   const fetchNotes = async () => {
     try {
       const notesData = await API.graphql({
@@ -36,12 +68,33 @@ function App() {
     }
   };
 
+  const onChange = ({ target: { value, name } }) => {
+    dispatch({ type: 'SET_INPUT', name, value });
+  };
+
   useEffect(() => {
     fetchNotes();
   }, []);
 
   return (
     <div style={styles.container}>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder='Note name'
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder='Note description'
+        name='description'
+        style={styles.input}
+      />
+      <Button onClick={createNote} type='primary'>
+        Create Note
+      </Button>
       <List
         loading={state.loading}
         dataSource={state.notes}
